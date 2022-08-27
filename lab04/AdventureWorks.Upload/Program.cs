e77cf2435765f7e4d6b9844098aa46511affedc6
Antanas Domarkas <antanas.domarkas@curonia.lt>
2022-08-28 02:17:34 +0300

    finished data upload app

--- lab04/AdventureWorks.Upload/Program.cs
+++ lab04/AdventureWorks.Upload/Program.cs
@@ -11,7 +11,7 @@ namespace AdventureWorks.Upload
 		private static string _authorizationKey = "";
 		private static string _databaseName = "";
 		private static string _containerName = "";
-		private static string _primaryKey = "";
+		private static string _partitionKey = "";
 		private static string _jsonFilePath = "";
 
 		private static int _amountToInsert;
@@ -33,15 +33,15 @@ namespace AdventureWorks.Upload
 
 				// Configure indexing policy to exclude all attributes to maximize RU/s usage
 				await Console.Out.WriteLineAsync("Creating a container if not already exists...");
-				await database.DefineContainer(_containerName, _primaryKey)
+				await database.DefineContainer(_containerName, _partitionKey)
 					.WithIndexingPolicy()
-					.WithIndexingMode(IndexingMode.Consistent)
-					.WithIncludedPaths()
-					.Attach()
-					.WithExcludedPaths()
-					.Path("/*")
-					.Attach()
-					.Attach()
+						.WithIndexingMode(IndexingMode.Consistent)
+							.WithIncludedPaths()
+							.Attach()
+							.WithExcludedPaths()
+								.Path("/*")
+							.Attach()
+						.Attach()
 					.CreateAsync();
 
 				using StreamReader reader = new(File.OpenRead(_jsonFilePath));
@@ -63,22 +63,26 @@ namespace AdventureWorks.Upload
 				List<Task> tasks = new(_amountToInsert);
 				foreach (var model in _models)
 				{
-					tasks.Add(container.CreateItemAsync(model, new PartitionKey(model.Category))
-						.ContinueWith(itemResponse =>
-						{
-							if (itemResponse.IsCompletedSuccessfully) return;
+					tasks.Add(
+						container
+							.CreateItemAsync(model, new PartitionKey(model.Category))
+							.ContinueWith(itemResponse =>
+								{
+									if (itemResponse.IsCompletedSuccessfully) return;
 
-							var innerExceptions = itemResponse.Exception?.Flatten() ?? new AggregateException();
-							if (innerExceptions.InnerExceptions
-								    .FirstOrDefault(innerEx => innerEx is CosmosException) is CosmosException cosmosException)
-							{
-								Console.Out.WriteLine($"Received {cosmosException.StatusCode} ({cosmosException.Message}).");
-							}
-							else
-							{
-								Console.Out.WriteLine($"Exception {innerExceptions.InnerExceptions.FirstOrDefault()}.");
-							}
-						}));
+									var innerExceptions = itemResponse.Exception?.Flatten() ?? new AggregateException();
+									if (innerExceptions.InnerExceptions
+										    .FirstOrDefault(innerEx => innerEx is CosmosException) is CosmosException cosmosException)
+									{
+										Console.Out.WriteLine($"Received {cosmosException.StatusCode} ({cosmosException.Message}).");
+									}
+									else
+									{
+										Console.Out.WriteLine($"Exception {innerExceptions.InnerExceptions.FirstOrDefault()}.");
+									}
+								}
+							)
+					);
 				}
 
 				// Wait until all are done
@@ -103,7 +107,7 @@ namespace AdventureWorks.Upload
 			_endpointUrl = config.GetRequiredSection("EndpointUrl").Value;
 			_databaseName = config.GetRequiredSection("DatabaseName").Value;
 			_containerName = config.GetRequiredSection("ContainerName").Value;
-			_primaryKey = config.GetRequiredSection("PrimaryKey").Value;
+			_partitionKey = config.GetRequiredSection("PartitionKey").Value;
 			_jsonFilePath = config.GetRequiredSection("JSONFilePath").Value;
 			// Value should be stored in environment variable Adv_AuthorizationKey
 			_authorizationKey = config.GetRequiredSection("AuthorizationKey").Value;
