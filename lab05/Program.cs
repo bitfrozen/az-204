8201c73f481f8a9181093ba09f4dca933ea84b36
Antanas Domarkas <antanas.domarkas@curonia.lt>
2022-09-02 11:05:47 +0300

    adding lab05

---
+++ lab05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace ipcheck;
+
+public static class Program
+{
+	public static void Main()
+	{
+		// Check if network is available
+		if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+		{
+			Console.WriteLine("Current IP Addresses:");
+
+			// Get host entry for current hostname
+			string hostname = Dns.GetHostName();
+			IPHostEntry host = Dns.GetHostEntry(hostname);
+
+			// Iterate over each IP address and render their values
+			foreach (var address in host.AddressList)
+			{
+				Console.WriteLine($"\t{address}");
+			}
+		}
+		else
+		{
+			Console.WriteLine("No network connection");
+		}
+	}
+}
