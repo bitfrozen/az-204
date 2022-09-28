9d97cce5fa35a4b6d54da5c60f51034badef60ca
Antanas Domarkas <antanas.domarkas@curonia.lt>
2022-09-28 22:04:59 +0300

    lab10

---
+++ lab10/MessagePublisher/Program.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace MessagePublisher;
+
+public class Program
+{
+	private static string _storageConnectionString = "";
+	private static string _queueName = "";
+	private static int _numberOfMessages = 0;
+	private static ServiceBusClient _client = default!;
+	private static ServiceBusSender _sender = default!;
+
+	public static async Task Main()
+	{
+		GetConfiguration();
+		_client = new ServiceBusClient(_storageConnectionString);
+		_sender = _client.CreateSender(_queueName);
+		using ServiceBusMessageBatch messageBatch = await _sender.CreateMessageBatchAsync();
+		for (var i = 1; i <= _numberOfMessages; i++)
+		{
+			if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+			{
+				throw new Exception($"The message {i} is too large to fit in the batch.");
+			}
+		}
+
+		try
+		{
+			await _sender.SendMessagesAsync(messageBatch);
+			Console.WriteLine($"A batch of {_numberOfMessages} messages has been published to the queue.");
+		}
+		finally
+		{
+			await _sender.DisposeAsync();
+			await _client.DisposeAsync();
+		}
+	}
+	
+	private static void GetConfiguration()
+	{
+		IConfigurationBuilder? configuration = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile($"appsettings.json")
+			.AddUserSecrets<Program>();
+		IConfigurationRoot? config = configuration.Build();
+		_storageConnectionString = config.GetRequiredSection("StorageConnectionString").Value;
+		_queueName = config.GetRequiredSection("QueueName").Value;
+		_numberOfMessages = Convert.ToInt32(config.GetRequiredSection("NumberOfMessages").Value);
+	}
+}
---
+++ lab10/MessageReader/Program.cs
@@ -0,0 +1,60 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace MessageReader;
+
+public class Program
+{
+	private static string _storageConnectionString = "";
+	private static string _queueName = "";
+	private static ServiceBusClient _client = default!;
+	private static ServiceBusProcessor _processor = default!;
+
+	public static async Task Main()
+	{
+		GetConfiguration();
+		_client = new ServiceBusClient(_storageConnectionString);
+		_processor = _client.CreateProcessor(_queueName, new ServiceBusProcessorOptions());
+
+		try
+		{
+			_processor.ProcessMessageAsync += MessageHandler;
+			_processor.ProcessErrorAsync += ErrorHandler;
+			await _processor.StartProcessingAsync();
+			Console.WriteLine("Wait for a minute and then press any key to end the processing");
+			Console.ReadKey();
+			Console.WriteLine("\nStopping the receiver...");
+			await _processor.StopProcessingAsync();
+			Console.WriteLine("Stopped receiving messages");
+		}
+		finally
+		{
+			await _processor.DisposeAsync();
+			await _client.DisposeAsync();
+		}
+	}
+
+	private static async Task MessageHandler(ProcessMessageEventArgs args)
+	{
+		var body = args.Message.Body.ToString();
+		Console.WriteLine($"Received: {body}");
+		await args.CompleteMessageAsync(args.Message);
+	}
+
+	private static Task ErrorHandler(ProcessErrorEventArgs args)
+	{
+		Console.WriteLine(args.Exception.ToString());
+		return Task.CompletedTask;
+	}
+
+	private static void GetConfiguration()
+	{
+		IConfigurationBuilder? configuration = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile($"appsettings.json")
+			.AddUserSecrets<Program>();
+		IConfigurationRoot? config = configuration.Build();
+		_storageConnectionString = config.GetRequiredSection("StorageConnectionString").Value;
+		_queueName = config.GetRequiredSection("QueueName").Value;
+	}
+}
